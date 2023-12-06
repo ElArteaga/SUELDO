ba39b39eb9daa675be94feed7db1dbfb3a822cc7
DIEGO ERICK <DIEGO ERICK@LAPTOP-1BPHSF7N>
2023-12-06 12:26:02 -0600

    Agregar archivos de proyecto.

---
+++ SUELDO/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace SUELDO
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(12, 146);
+            label2.Name = "label2";
+            label2.Size = new Size(124, 17);
+            label2.TabIndex = 1;
+            label2.Text = "INGRESAR SUELDO";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(142, 146);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(137, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(12, 241);
+            label3.Name = "label3";
+            label3.Size = new Size(107, 17);
+            label3.TabIndex = 3;
+            label3.Text = "NUEVO SUELDO";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(125, 241);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(154, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(44, 189);
+            button1.Name = "button1";
+            button1.Size = new Size(92, 31);
+            button1.TabIndex = 5;
+            button1.Text = "DECISION";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(157, 189);
+            button2.Name = "button2";
+            button2.Size = new Size(88, 31);
+            button2.TabIndex = 6;
+            button2.Text = "BORRAR";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(287, 100);
+            label1.TabIndex = 7;
+            label1.Text = "DADO COMO DATO EL SUELDO DE UN \r\nTRABAJADOR, EL PROGRAMA APLICA \r\nUN AUMENTO DEL 15% SI EL SUELDO \r\nES INFERIOR A $400,000.\r\n\r\n";
+            label1.Click += label1_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(192, 255, 255);
+            ClientSize = new Size(312, 286);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "SUELDO";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+    }
+}
---
+++ SUELDO/Form1.cs
@@ -0,0 +1,38 @@
+namespace SUELDO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sueldo, aum, nsue;
+            sueldo = double.Parse(textBox1.Text);
+            if (sueldo < 400000.00)
+            {
+                aum = sueldo * 0.15;
+                nsue = sueldo + aum;
+                textBox2.Text = nsue.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+    }
+}
